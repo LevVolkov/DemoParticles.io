e6318d2349aca7b6f6bee6d330eb41a98fcdbc52
Лев Волков <88002142+LevVolkov@users.noreply.github.com>
2024-02-26 00:39:53 +0300

    Update particleground.js

--- assets/particleground.js
+++ assets/particleground.js
@@ -2,8 +2,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#778899' ,
-    lineColor:'#778899' 
+    dotColor: '#00FF00' ,
+    lineColor:'#00FF00' 
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
